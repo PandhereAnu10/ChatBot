706f30a13eac3722978361e0425dc89e6c70c905
PandhereAnu@10 <anushka.pandhere10@gmail.com>
2023-03-28 15:52:29 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+// 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
+
+const { Configuration, OpenAIApi } = require("openai");
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const configuration = new Configuration({
+    organization: "org-87OoqT6pzuSWfWe2gfYWl4DP",
+    apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
+
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+
+
+const port = 3080
+
+app.post('/', async (req,res) => {
+    const { message, currentModel} = req.body;
+    console.log(message, "message")
+    console.log(currentModel, "currentModel")
+    const response = await openai.createCompletion({
+        model: `${currentModel}` ,// "text-davinci-003"
+        prompt: `${message}`,
+        max_tokens: 100,
+        temperature: 0.5,
+      });
+    res.json({
+        message: response.data.choices[0].text,
+    })
+});
+
+app.get('/models', async (req,res) => {
+    
+    const response = await openai.listEngines();
+    console.log(response.data.data)
+    res.json({
+      models: response.data.data  
+    })
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+});
